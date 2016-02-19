ca023af1459c94bb4152ab8c5234d02c1d82c9e0
Mingriweiji-github <18046506190@163.com>
2016-02-19 14:11:43 +0800

    Initial Commit

---
+++ SwitfDemo1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwitfDemo1
+//
+//  Created by Mr.Li on 16/2/19.
+//  Copyright © 2016年 Mr.Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
